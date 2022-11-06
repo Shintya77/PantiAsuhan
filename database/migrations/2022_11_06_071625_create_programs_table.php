4f52c2db4e790fccab2187e964ba3e7f71909266
= <=>
2022-11-06 15:00:17 +0700

    tabel binaan & program

---
+++ database/migrations/2022_11_06_071625_create_programs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProgramsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('programs', function (Blueprint $table) {
+            $table->id('id_program');
+            $table->string('nama_program');
+            $table->string('dns_butuh');
+            $table->string('dns_terkumpul');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('programs');
+    }
+}
