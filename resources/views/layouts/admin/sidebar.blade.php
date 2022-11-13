6056ee7ce03d485715c72f3a0218f30e6e81fd23
= <=>
2022-11-13 20:43:33 +0700

    menghapus edit donatur

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -70,7 +70,7 @@
             </ul>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="#" class="nav-link ">
+                <a href="{{route('struktur.index')}}" class="nav-link ">
                   <i class="far fa-user nav-icon"></i>
                   <p>Edit Struktur</p>
                 </a>
